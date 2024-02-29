a16ad3d93688e18b6f4addfb0fa5b1fedfee3742
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2024-02-29 16:43:01 +0530

    Update

---
+++ src/main/java/com/example/crud/ReactSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringBootCrudApplication.class, args);
+	}
+
+}
